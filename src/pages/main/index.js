c7a2a605382d30183b4b9cc9d60b1ce554918f6a
cadumancini <cadumancini@gmail.com>
2020-04-15 11:54:24 -0300

    Added page navigation

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -6,19 +6,42 @@ import './styles.css'
 export default class Main extends Component {
 state = {
   products: [],
+  productInfo: {},
+  page: 1,
 }
 
   componentDidMount() {
     this.loadProducts();
   }
 
-  loadProducts = async () => { // arrow function doesn't overwrite value of 'this'
-    const response = await api.get('/products');
-    this.setState({ products: response.data.docs });
+  loadProducts = async (page = 1) => { // arrow function doesn't overwrite value of 'this'
+    const response = await api.get(`/products?page=${page}`);
+    
+    const { docs, ...productInfo } = response.data;
+
+    this.setState({ products: docs, productInfo, page });
+  }
+
+  prevPage = () => {
+    const { page } = this.state;
+
+    if(page === 1) return;
+
+    const pageNumber = page - 1;
+    this.loadProducts(pageNumber);
+  }
+
+  nextPage = () => {
+    const { page, productInfo } = this.state;
+
+    if(page === productInfo.pages) return;
+
+    const pageNumber = page + 1;
+    this.loadProducts(pageNumber);
   }
 
   render() {
-    const { products } = this.state;
+    const { products, page, productInfo } = this.state;
     return (
       <div className='product-list'>
         {products.map(product => (
@@ -29,6 +52,10 @@ state = {
             <a href=''>Access</a>
           </article>
         ))}
+        <div className='actions'>
+          <button disabled={page === 1} onClick={this.prevPage}>Previous</button>
+          <button disabled={page === productInfo.pages} onClick={this.nextPage}>Next</button>
+        </div>
       </div>
     ); 
   }
